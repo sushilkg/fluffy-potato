76e318550e278ea28a86c04e8fc2e619b4f26d04
Sushil Gupta <desushil@gmail.com>
2018-10-11 19:10:14 +0545

    Updated sql query to be exactly like the requirement

--- src/TableJoin.php
+++ src/TableJoin.php
@@ -13,14 +13,15 @@ class TableJoin
 
     public function getSummary()
     {
-        $sql = 'SELECT customer.customer_id, customer.name, sale_order.order_id, grand_total.sum_total_amount
-FROM customer
-LEFT JOIN sale_order
-ON sale_order.customer_id = customer.customer_id
-LEFT JOIN (SELECT order_id, SUM(total_amount) as sum_total_amount FROM sale_order_item GROUP BY order_id) AS grand_total
-ON grand_total.order_id = sale_order.order_id
-GROUP BY customer.name
-ORDER BY grand_total.sum_total_amount DESC;';
+        $sql = 'SELECT customer.customer_id, customer.name,
+                SUM(CASE WHEN sale_order_item.total_amount IS NOT NULL THEN sale_order_item.total_amount ELSE 0 END) AS sum_total_amount
+                FROM customer
+                LEFT JOIN sale_order
+                ON sale_order.customer_id = customer.customer_id
+                LEFT JOIN sale_order_item
+                ON sale_order_item.order_id = sale_order.order_id
+                GROUP BY customer.name
+                ORDER BY sum_total_amount DESC;';
 
         return $this->database->query($sql)->fetchAll();
     }
