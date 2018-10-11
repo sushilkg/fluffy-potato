0b7a16c4558601a80b6b76eed42b7de4d820b0fb
Sushil Gupta <desushil@gmail.com>
2018-10-11 12:58:52 +0545

    Reorganized html

---
+++ assets/truck.js
@@ -0,0 +1,22 @@
+$(document).keydown(function (event) {
+    switch (event.keyCode) {
+        case 37:
+            changeDirection("left");
+            break;
+        case 39:
+            changeDirection("right");
+            break;
+    }
+});
+
+function changeDirection(direction) {
+    $('.truck_wheel').each(function () {
+        if (direction === "right") {
+            $(this).css('animation', "rotateRight 4s infinite linear");
+            $('.truck_container').css('animation', "goRight 30s infinite linear normal");
+        } else {
+            $(this).css('animation', "rotateLeft 4s infinite linear");
+            $('.truck_container').css('animation', "goLeft 30s infinite linear normal");
+        }
+    });
+}
