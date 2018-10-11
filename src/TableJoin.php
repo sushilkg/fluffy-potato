7c35ede4a9c5688cfe71591df79f768b86c77e96
Sushil Gupta <desushil@gmail.com>
2018-10-11 17:55:04 +0545

    Improved sql query - but can be improved further

--- src/TableJoin.php
+++ src/TableJoin.php
@@ -13,6 +13,15 @@ class TableJoin
 
     public function getSummary()
     {
-        $summary = $this->database->query("SELECT * FROM customer");
+        $sql = 'SELECT customer.customer_id, customer.name, sale_order.order_id, grand_total.sum_total_amount
+FROM customer
+LEFT JOIN sale_order
+ON sale_order.customer_id = customer.customer_id
+LEFT JOIN (SELECT order_id, SUM(total_amount) as sum_total_amount FROM sale_order_item GROUP BY order_id) AS grand_total
+ON grand_total.order_id = sale_order.order_id
+GROUP BY customer.name
+ORDER BY grand_total.sum_total_amount DESC;';
+
+        return $this->database->query($sql)->fetchAll();
     }
 }
--- tableJoin.php
+++ tableJoin.php
@@ -5,4 +5,5 @@ namespace App;
 require "./vendor/autoload.php";
 
 $tableJoin = new TableJoin(new Database());
+echo "<pre>";
 print_r($tableJoin->getSummary());
