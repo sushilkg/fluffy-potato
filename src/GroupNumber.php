3acd8b14aebb443f074226d1679ed4c8d0d526e9
Sushil Gupta <desushil@gmail.com>
2018-10-11 20:29:05 +0545

    Excluding already used up number

--- src/GroupNumber.php
+++ src/GroupNumber.php
@@ -4,15 +4,24 @@ namespace App;
 
 class GroupNumber
 {
-    public function getGroupNumber($group_number)
+    protected $all_group_numbers = [];
+
+    public function getGroupNumber(int $group_number): array
     {
         $numbers = [];
-        for ($i = 1; $i <= 500; $i++) {
-            if ($i % $group_number === 0) {
+        for ($i = 1; $i < 500; $i++) {
+            if ($i % $group_number === 0 && !$this->alreadyUsedNumber($i)) {
                 $numbers[] = $i;
+                $this->all_group_numbers[] = $i;
             }
         }
 
         return $numbers;
     }
+
+    protected function alreadyUsedNumber($number_to_check)
+    {
+        return in_array($number_to_check, $this->all_group_numbers);
+    }
+
 }
