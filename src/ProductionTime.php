886c86347923ddf7bbf155cfaad59a5ab7be1cd9
Sushil Gupta <desushil@gmail.com>
2018-10-11 15:23:13 +0545

    Calculating next working date

--- src/ProductionTime.php
+++ src/ProductionTime.php
@@ -18,15 +18,36 @@ class ProductionTime
 
     public static function Delivery($order_date)
     {
-        $production_date = "";
-        $shipping_date   = "";
-        $delivery_date   = "";
+        $production_date = $order_date + self::getNextWorkingDate("production", $order_date + "one date");
+        $delivery_date   = $production_date + self::getNextWorkingDate("shipping", $order_date + "one date");;
 
         return $delivery_date;
     }
 
+    protected static function getNextWorkingDate($team, $date)
+    {
+        if (self::isWorkingDate($team, $date)) {
+            return $date;
+        } else {
+            return self::getNextWorkingDate($team, $date + "one day");
+        }
+    }
+
     protected static function isWorkingDate($team, $date)
     {
+        switch ($team) {
+            case "production":
+                $holiday = self::isHoliday($date) || self::isSaturday($date) || self::isSunday($date);
+                break;
+            case "shipping":
+                $holiday = self::isHoliday($date) || self::isSaturday($date);
+                break;
+            default:
+                $holiday = false;
+                break;
+        }
+
+        return $holiday;
     }
 
     protected static function isHoliday($date)
