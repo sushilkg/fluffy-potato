373da9860a0b3d103fbcedea37ad5306955da9b5
Sushil Gupta <desushil@gmail.com>
2018-10-18 11:15:58 +0545

    Minor refactored database class

--- src/Database.php
+++ src/Database.php
@@ -4,42 +4,38 @@ namespace App;
 
 class Database
 {
-    public static $connection = null;
+    const HOST     = '127.0.0.1';
+    const USER     = 'root';
+    const PASS     = 'root';
+    const DATABASE = 'gogoprint';
+    const CHARSET  = 'utf8mb4';
+
+    protected static $connection = null;
 
     public function __construct()
     {
         if (!self::$connection) {
-            self::connection();
+            self::connect();
         }
     }
 
-    private static function connection()
+    protected static function connect()
     {
-        $host    = '127.0.0.1';
-        $db      = 'gogoprint';
-        $user    = 'root';
-        $pass    = 'root';
-        $charset = 'utf8mb4';
-
-        $dsn     = "mysql:host=$host;dbname=$db;charset=$charset";
+        $dsn     = "mysql:host=".self::HOST.";dbname=".self::DATABASE.";charset=".self::CHARSET;
         $options = [
             \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
             \PDO::ATTR_EMULATE_PREPARES   => false,
         ];
         try {
-            self::$connection = new \PDO($dsn, $user, $pass, $options);
+            self::$connection = new \PDO($dsn, self::USER, self::PASS, $options);
         } catch (\PDOException $e) {
             throw new \PDOException($e->getMessage(), (int) $e->getCode());
         }
     }
 
-    public function getConnection()
+    public function query($sql, $method = \PDO::FETCH_ASSOC)
     {
-        return self::$connection;
-    }
-
-    public function query($sql, $method = \PDO::FETCH_ASSOC) {
         try {
             return self::$connection->query($sql, $method);
         } catch (\PDOException $exception) {
