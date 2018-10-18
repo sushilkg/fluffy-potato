1b5bd2928d3de6ec0962b4c9d59394c002d0752c
Sushil Gupta <desushil@gmail.com>
2018-10-18 11:42:12 +0545

    Minor refactor: Moved the direction reversal jquery selector outside of each loop

--- assets/truck.js
+++ assets/truck.js
@@ -13,10 +13,14 @@ function changeDirection(direction) {
     $('.truck_wheel').each(function () {
         if (direction === "right") {
             $(this).css('animation', "rotateRight 4s infinite linear");
-            $('.truck_container').css('animation', "moveTruck 30s infinite linear normal");
         } else {
             $(this).css('animation', "rotateLeft 4s infinite linear");
-            $('.truck_container').css('animation', "moveTruck 30s infinite linear reverse");
         }
     });
+
+    if(direction === "right") {
+        $('.truck_container').css('animation', "moveTruck 30s infinite linear normal");
+    } else {
+        $('.truck_container').css('animation', "moveTruck 30s infinite linear reverse");
+    }
 }
